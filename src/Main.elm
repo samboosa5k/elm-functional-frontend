1fd70f47b47e6d36ac616429f1cde447e1a56eb2
Jasper V <jasper.verbon@gmail.com>
2023-12-09 20:36:03 +0100

    [component/test] trunk - Attempt at combining components

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,60 @@
-module Main exposing (Model, init)
+module Main exposing (main)
 
-import Image
+import Html exposing (Html, div, h1, section, text)
 
 
 
--- MAIN
+-- VIEW
+
+
+type alias TitleModel =
+    { text : String
+    }
+
+
+titleInit : TitleModel
+titleInit =
+    { text = "This is the title"
+    }
+
+
+title : TitleModel -> Html msg
+title titleModel =
+    h1 [] [ text titleModel.text ]
+
+
+
 -- MODEL
 
 
-type alias Model =
-    { imageFocus : Image.Model
-    , galleryData : List Image.Model
+type alias ContentModel =
+    { title : String
+    , text : String
     }
+
+
+contentInit : ContentModel
+contentInit =
+    { title = "App Title"
+    , text = "Nested app text"
+    }
+
+
+
+-- VIEW
+
+
+content : ContentModel -> Html msg
+content contentModel =
+    div []
+        [ div [] [ text contentModel.title ]
+        , div [] [ text contentModel.text ]
+        ]
+
+
+main : Html msg
+main =
+    section []
+        [ title titleInit
+        , content contentInit
+        ]
