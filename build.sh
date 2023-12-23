4131e569c1120c107126aaf0ffc5cf567dc4d211
Jasper V <jasper.verbon@gmail.com>
2023-12-23 20:50:50 +0100

    [script] trunk - Added build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-elm make src/Main.elm --output=elm.js
+#!/bin/bash
+
+function build() {
+
+    local output_name="elm.js"
+    local command="elm make src/Main.elm --output=$output_name"
+
+    $command
+}
+
+alias build=build
+build
