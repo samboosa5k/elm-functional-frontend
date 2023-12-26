4451ba2bcd91cd08325feaacf6d0450946421974
Jasper V <jasper.verbon@gmail.com>
2023-12-26 23:25:21 +0100

    [script] trunk - Monitor/watch script to automatically compile scss

--- monitor.sh
+++ monitor.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # error="./scripts/error.sh"
+log="./scripts/log.sh"
 build_app="./build.sh"
 build_styles="./build_styles.sh"
 
-function watch_changes() {
-    # watch for changes in ./styles
-    # log a list of files that changed e.g. $log "./src/index.js"
-
-    # watch for changes to scss files in ./styles
-    # log a list of files that changed e.g. echo "./styles/index.scss"
-    # run $build_styles
-    watch -n 1 -d -t -g ./styles/ -e scss -c "$build_styles"
+function monitor() {
+    echo "Monitoring..."
+    # use inotifywait to monitor changes at intervals of 5 seconds in the directory ./styles to .scss files
+    # and if there is a change, print the filename of the modified file in intervals of 5 seconds
+    # if there is no change, then the script will wait for 5 seconds and then check again
+    while inotifywait -q -r -e modify ./styles; do
+        echo "Change detected"
+        # if there is a change, then run the build_styles.sh script
+        $build_styles
+        # if the build_styles.sh script is successful, then run the build.sh script
+        #        if [ $? -eq 0 ]; then
+        #            build_app
+        #        fi
+    done
 }
 
-alias watch_changes=watch_changes
-watch_changes
+alias monitor=monitor
+monitor
