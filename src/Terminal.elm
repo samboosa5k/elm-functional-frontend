3e00bf7e1c82c004c7c8ba9d2237a9b067b0537b
Jasper V <jasper.verbon@gmail.com>
2024-01-06 13:42:02 +0100

    [view] simplify-app - View interface for terminal separated from Main module logic

--- src/Terminal.elm
+++ src/Terminal.elm
@@ -1,16 +1,36 @@
-module Terminal exposing (view)
+module Terminal exposing (outputView, view)
 
-import Html exposing (input)
+import Html exposing (Attribute, Html, div, input, p, text)
 import Html.Attributes exposing (id, placeholder, value)
-import Html.Events exposing (onInput)
+import Html.Events exposing (keyCode, on, onInput)
+import Json.Decode as Json
 
 
-view : { a | userInput : String } -> (String -> msg) -> Html.Html msg
-view { userInput } handleUserInput =
+type alias OutputLine =
+    { inputCommand : String
+    , outputResponse : String
+    }
+
+
+outputView : List OutputLine -> List (Html msg)
+outputView model =
+    List.map
+        (\x -> div [] [ p [] [ text x.inputCommand ], p [] [ text x.outputResponse ] ])
+        model
+
+
+onKeyDown : (Int -> msg) -> Attribute msg
+onKeyDown tagger =
+    on "keydown" (Json.map tagger keyCode)
+
+
+view : { a | userInput : String } -> (String -> msg) -> (Int -> msg) -> Html.Html msg
+view { userInput } handleUserInput handleKeyDown =
     input
         [ id "terminal__input"
         , placeholder "please enter a command"
         , value userInput
         , onInput handleUserInput
+        , onKeyDown handleKeyDown
         ]
         []
