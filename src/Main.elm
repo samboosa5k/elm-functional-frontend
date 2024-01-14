2b402f4f60cd9a2535908b685e305e3b72fe2812
Jasper V <jasper.verbon@gmail.com>
2024-01-14 22:59:02 +0100

    [refactor] simplify-app - Separated Input field logic to module

--- src/Main.elm
+++ src/Main.elm
@@ -2,23 +2,19 @@ module Main exposing (main)
 
 import Array exposing (fromList, push, toList)
 import Browser exposing (Document)
-import Html exposing (Attribute, Html, div, header, input, main_, pre)
-import Html.Attributes exposing (class, id, placeholder, value)
-import Html.Events exposing (keyCode, on, onInput)
-import Json.Decode as Json
+import Html exposing (div, header, main_, pre)
+import Html.Attributes exposing (class, id)
+import Html.Events exposing (keyCode)
+import Input
 import List.Extra
 import Platform.Cmd as Cmd exposing (Cmd)
 import Terminal
 
 
-type alias InputModel =
-    { id_ : Int, content : String, outputResponse : String }
-
-
 type alias Model =
     { userInput : String
     , terminalOutput : List { inputCommand : String, outputResponse : String }
-    , inputWindows : List InputModel
+    , inputWindows : List Input.Model
     }
 
 
@@ -39,18 +35,12 @@ initialState =
     }
 
 
-type WindowMsg
-    = HandleWindowInput String
-    | HandleWindowKeydown Int
-    | HandleWindowEnter
-
-
 type Msg
     = NoOp
     | HandleUserInput String
     | HandleKeydown Int
     | HandleEnter
-    | UpdateWindow Int WindowMsg
+    | UpdateWindow Int Input.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -101,68 +91,12 @@ update msg model =
                             { id_ = 0, content = "", outputResponse = "" }
 
                 ( updatedInput, cmdMsg ) =
-                    updateInput windowMsg selectedInput
+                    Input.update windowMsg selectedInput
 
                 inputs =
                     List.Extra.setAt index updatedInput model.inputWindows
             in
             ( { model | inputWindows = inputs }, Cmd.map (UpdateWindow indexId) cmdMsg )
-
-
-updateInput : WindowMsg -> InputModel -> ( InputModel, Cmd WindowMsg )
-updateInput windowMsg inputModel =
-    case windowMsg of
-        HandleWindowInput inputContent ->
-            ( { inputModel | content = inputContent }, Cmd.none )
-
-        HandleWindowEnter ->
-            ( { inputModel | content = "", outputResponse = inputModel.content }, Cmd.none )
-
-        HandleWindowKeydown keyCode ->
-            if keyCode == 13 then
-                updateInput HandleWindowEnter inputModel
-
-            else
-                ( inputModel, Cmd.none )
-
-
-onKeyDown : (Int -> msg) -> Attribute msg
-onKeyDown tagger =
-    on "keydown" (Json.map tagger keyCode)
-
-
-multiInputField : InputModel -> (String -> msg) -> (Int -> msg) -> Html msg
-multiInputField { id_, content } handleInput handleKeyDown =
-    input
-        [ id (String.fromInt id_)
-        , class "terminal-input__field"
-        , placeholder "Please enter a command"
-        , value content
-        , onInput handleInput
-        , onKeyDown handleKeyDown
-        ]
-        []
-
-
-
--- REFERENCE: https://blog.revathskumar.com/2018/05/elm-message-passing-between-modules.html
--- ARTICLE AUTHOR: https://github.com/revathskumar
-
-
-multiInput : List InputModel -> Html Msg
-multiInput listInputs =
-    div [ class "terminal-input" ]
-        (if List.length listInputs > 0 then
-            List.map
-                (\listInput ->
-                    Html.map (UpdateWindow listInput.id_)
-                        (multiInputField listInput HandleWindowInput HandleWindowKeydown)
-                )
-                listInputs
-
-         else
-            [ input [ id "first-input", class "terminal-input__field", placeholder "Please enter a command", value "" ] [] ]
-        )
 
 
 view : Model -> Document Msg
@@ -188,7 +122,7 @@ view { userInput, terminalOutput, inputWindows } =
                         HandleUserInput
                         HandleKeydown
                     ]
-                , div [ class "main-content__block" ] [ multiInput inputWindows ]
+                , div [ class "main-content__block" ] [ Input.multiInput inputWindows UpdateWindow ]
                 ]
             ]
         ]
